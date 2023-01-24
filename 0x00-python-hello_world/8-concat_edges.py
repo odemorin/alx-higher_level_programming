5dfb7a55bed76305938f713cd81df8a5291f6d8d
odemorin <tundeadeolu@gmail.com>
2023-01-24 14:50:13 +0000

    tt

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
-str = "Python is an interpreted, interactive, object-oriented programming\
+word = "Holberton"
 
- language that combines remarkable power with very clear syntax"
+word_first_3 = word[:3]
 
-str = str[39:67] + str[-22:-17] + str[0:6]
+word_last_2 = word[-2:]
 
-print(str)
+middle_word = word[1:-1]
+
+print("First 3 letters: {}".format(word_first_3))
+
+print("Last 2 letters: {}".format(word_last_2))
+
+print("Middle word: {}".format(middle_word))
